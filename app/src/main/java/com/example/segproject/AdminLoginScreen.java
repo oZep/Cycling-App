214c1d07dc72293a3c16c5a2ffc809929c95f19e
Michelle <0101m744@gmail.com>
2023-10-10 19:22:48 -0400

    Added admin login screen, UI adjustment

--- app/src/main/java/com/example/segproject/AdminLoginScreen.java
+++ app/src/main/java/com/example/segproject/AdminLoginScreen.java
@@ -1,14 +1,23 @@
 package com.example.segproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+<<<<<<< HEAD
+=======
+
+>>>>>>> 0f097f0 (Added admin login screen, UI adjustment)
 import android.os.Bundle;
 
 public class AdminLoginScreen extends AppCompatActivity {
 
+<<<<<<< HEAD
     Button buttonLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         buttonLogin = findViewById(R.id.btn_login);
+=======
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+>>>>>>> 0f097f0 (Added admin login screen, UI adjustment)
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login_screen);
     }
