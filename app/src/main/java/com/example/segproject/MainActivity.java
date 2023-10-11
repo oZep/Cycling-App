71d83ed51efcfcec9b48c7473820b7d9a1f81ac6
Destitution <squidkiller64@hotmail.com>
2023-10-10 21:53:25 -0400

    this

--- app/src/main/java/com/example/segproject/MainActivity.java
+++ app/src/main/java/com/example/segproject/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.segproject;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 public class MainActivity extends AppCompatActivity {
-    public void login(String name, String pass) {
-        if (name.equals("username") && pass.equals("password")) {
+    public void login(String name, String pass, boolean Admin) {
+        if (name.equals("username") && pass.equals("password") && !Admin) {
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        }
+        else if (name.equals("username") && pass.equals("password")) {
+            Intent intent = new Intent(getApplicationContext(), Registration.class);
         }
     }
 }
