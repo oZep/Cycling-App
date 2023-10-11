9750d91b1114d8ffb5ef9712e4c16e5713f8421a
Destitution <squidkiller64@hotmail.com>
2023-10-10 21:51:10 -0400

    this

--- app/src/main/java/com/example/segproject/MainActivity.java
+++ app/src/main/java/com/example/segproject/MainActivity.java
@@ -1,21 +1,11 @@
 package com.example.segproject;
 
-import android.os.Bundle;
-import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // Get the welcome text from the Intent
-        String welcomeText = getIntent().getStringExtra("WELCOME_TEXT");
-
-        // Update the UI element with the welcome text
-        TextView welcomeTextAny = findViewById(R.id.welcome_text);
-        welcomeTextAny.setText(welcomeText);
+    public void login(String name, String pass) {
+        if (name.equals("username") && pass.equals("password")) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        }
     }
-
 }
