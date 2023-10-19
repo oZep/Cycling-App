f6a9df80987ca44ecd3f18912237d14121e4d800
pkuottawa <145950345+pkuottawa@users.noreply.github.com>
2023-10-18 21:45:26 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/firebaseapp2/MainActivity.java
+++ app/src/main/java/com/example/firebaseapp2/MainActivity.java
@@ -35,14 +35,11 @@ public class MainActivity extends AppCompatActivity {
 
     List<Product> products;
 
-    DatabaseReference databaseProducts;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        databaseProducts = FirebaseDatabase.getInstance().getReference("products");
         editTextName = (EditText) findViewById(R.id.editTextName);
         editTextPrice = (EditText) findViewById(R.id.editTextPrice);
         listViewProducts = (ListView) findViewById(R.id.listViewProducts);
@@ -72,33 +69,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        //attaching value event listener
-        databaseProducts.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-
-                //clearing the previous artist list
-                products.clear();
-
-                //iterating through all the nodes
-                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
-                    //getting product
-                    Product product = postSnapshot.getValue(Product.class);
-                    //adding product to the list
-                    products.add(product);
-                }
-
-                //creating adapter
-                ProductList productsAdapter = new ProductList(MainActivity.this, products);
-                //attaching adapter to the listview
-                listViewProducts.setAdapter(productsAdapter);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
     }
 
 
@@ -117,7 +87,6 @@ public class MainActivity extends AppCompatActivity {
         dialogBuilder.setTitle(productName);
         final AlertDialog b = dialogBuilder.create();
         b.show();
-
 
         buttonUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -141,50 +110,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateProduct(String id, String name, double price) {
-        //getting the specified product reference
-        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("products").child(id);
-        //updating product
-        Product product = new Product(id, name, price);
-        dR.setValue(product);
-        Toast.makeText(getApplicationContext(), "Product Updated", Toast.LENGTH_LONG).show();
+
+        Toast.makeText(getApplicationContext(), "NOT IMPLEMENTED YET", Toast.LENGTH_LONG).show();
     }
 
-    private boolean deleteProduct(String id) {
-        //getting the specified product reference
-        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("products").child(id);
-        //removing prodct
-        dR.removeValue();
-        Toast.makeText(getApplicationContext(), "Product Deleted", Toast.LENGTH_LONG).show();
-        return true;
+    private void deleteProduct(String id) {
+
+        Toast.makeText(getApplicationContext(), "NOT IMPLEMENTED YET", Toast.LENGTH_LONG).show();
     }
 
     private void addProduct() {
-        //getting the values to save
-        String name = editTextName.getText().toString().trim();
-        double price = Double.parseDouble(String.valueOf(editTextPrice.getText().toString()));
 
-        //checking if the value is provided
-        if (!TextUtils.isEmpty(name)) {
-
-            //getting a unique id using push().getKey() method
-            //it will create a unique id and we will use it as the Primary Key for our Product
-            String id = databaseProducts.push().getKey();
-
-            //creating an Product Object
-            Product product = new Product(id, name, price);
-
-            //Saving the Product
-            databaseProducts.child(id).setValue(product);
-
-            //setting edittext to blank again
-            editTextName.setText("");
-            editTextPrice.setText("");
-
-            //displaying a success toast
-            Toast.makeText(this, "Product added", Toast.LENGTH_LONG).show();
-        } else {
-            //if the value is not given displaying a toast
-            Toast.makeText(this, "Please enter a name", Toast.LENGTH_LONG).show();
-        }
+        Toast.makeText(this, "NOT IMPLEMENTED YET", Toast.LENGTH_LONG).show();
     }
 }
