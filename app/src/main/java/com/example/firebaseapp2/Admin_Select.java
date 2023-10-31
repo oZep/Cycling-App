b178a5542e7981d8bd1dc655e20e73ee3f139f55
joeyissa <joeyissa59@gmail.com>
2023-10-31 19:53:08 -0400

    Admmin page is done

--- app/src/main/java/com/example/firebaseapp2/Admin_Select.java
+++ app/src/main/java/com/example/firebaseapp2/Admin_Select.java
@@ -29,14 +29,13 @@ package com.example.firebaseapp2;
 
 public class Admin_Select extends AppCompatActivity {
 
-    EditText editTextName;
+    EditText editTextLocation;
+    EditText editTextIntensity;
 
     CheckBox TimeTrial;
     CheckBox HillClimb;
-    CheckBox RoadStageRace;
     CheckBox RoadRace;
     CheckBox GroupRides;
-    EditText editTextPrice;
     Button buttonAddProduct;
     ListView listViewProducts;
 
@@ -47,13 +46,12 @@ public class Admin_Select extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin_page);
 
-        //editTextName = (EditText) findViewById(R.id.editTextName);
-        //editTextPrice = (EditText) findViewById(R.id.editTextPrice);
+        editTextLocation = (EditText) findViewById(R.id.editTextName2);
+        editTextIntensity = (EditText) findViewById(R.id.editTextName3);
         listViewProducts = (ListView) findViewById(R.id.listViewProducts);
         buttonAddProduct = (Button) findViewById(R.id.addButton);
         TimeTrial = (CheckBox) findViewById(R.id.checkBox);
         HillClimb = (CheckBox) findViewById(R.id.checkBox2);
-        RoadStageRace  = (CheckBox) findViewById(R.id.checkBox3);
         RoadRace = (CheckBox) findViewById(R.id.checkBox4);
         GroupRides = (CheckBox) findViewById(R.id.checkBox5);
 
@@ -91,8 +89,8 @@ public class Admin_Select extends AppCompatActivity {
         final View dialogView = inflater.inflate(R.layout.update_dialog, null);
         dialogBuilder.setView(dialogView);
 
-        final EditText editTextName = (EditText) dialogView.findViewById(R.id.editTextName);
-        final EditText editTextPrice  = (EditText) dialogView.findViewById(R.id.editTextPrice);
+        //final EditText editTextLocation = (EditText) dialogView.findViewById(R.id.editTextName2);
+        //final EditText editTextIntensity  = (EditText) dialogView.findViewById(R.id.editTextName3);
         final Button buttonUpdate = (Button) dialogView.findViewById(R.id.buttonUpdateProduct);
         final Button buttonDelete = (Button) dialogView.findViewById(R.id.buttonDeleteProduct);
 
@@ -103,10 +101,11 @@ public class Admin_Select extends AppCompatActivity {
         buttonUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String name = editTextName.getText().toString().trim();
-                double price = Double.parseDouble(String.valueOf(editTextPrice.getText().toString()));
-                if (!TextUtils.isEmpty(name)) {
-                    updateProduct(productId, name, price);
+                String location = editTextLocation.getText().toString().trim();
+                double intensity = Double.parseDouble(String.valueOf(editTextIntensity.getText().toString()));
+                if (!TextUtils.isEmpty(location)) {
+                    //FIX
+                    //updateProduct(productId, location, intensity);
                     b.dismiss();
                 }
             }
@@ -121,17 +120,65 @@ public class Admin_Select extends AppCompatActivity {
         });
     }
 
-    private void updateProduct(String id, String name, double price) {
+    private void updateProduct(String id, String name, double intensity, String location) {
+        DatabaseReference dr = FirebaseDatabase.getInstance().getReference("products").child(id);
 
-        Toast.makeText(getApplicationContext(), "NOT IMPLEMENTED YET", Toast.LENGTH_LONG).show();
+        // FIX
+        //Product p = new Product(id, name, intensity, location);
+        //dr.setValue(p);
+
+        Toast.makeText(getApplicationContext(), "Product Updated", Toast.LENGTH_LONG).show();
     }
 
     private void deleteProduct(String id) {
+        DatabaseReference dr = FirebaseDatabase.getInstance().getReference("products").child(id);
 
-        Toast.makeText(getApplicationContext(), "NOT IMPLEMENTED YET", Toast.LENGTH_LONG).show();
+        dr.removeValue();
+        Toast.makeText(getApplicationContext(), "Product Deleted", Toast.LENGTH_LONG).show();
     }
 
+
     private void addProduct() {
+        TimeTrial = (CheckBox) findViewById(R.id.checkBox);
+        HillClimb = (CheckBox) findViewById(R.id.checkBox2);
+        RoadRace = (CheckBox) findViewById(R.id.checkBox4);
+        GroupRides = (CheckBox) findViewById(R.id.checkBox5);
+        String name;
+        if (GroupRides.isChecked()) {
+            name = "GroupRides";
+        }
+        else if ((RoadRace).isChecked()) {
+            name = "RoadRace";
+        }
+        else if (HillClimb.isChecked()) {
+            name = "HillClimb";
+        }
+        else if (TimeTrial.isChecked()) {
+            name = "TimeTrial";
+        }
+        else {
+            name = "ERROR activity not selected";
+        }
+
+        double Level = Double.parseDouble(String.valueOf(editTextIntensity.getText().toString()));
+        String Location = String.valueOf(editTextLocation.getText().toString());
+
+        if (!TextUtils.isEmpty(name)) {
+            //FIX
+            //String id = databaseProducts.push().getKey();
+            //Product p = new Product(id, name, level, location);
+
+            //databaseProducts.child(id).setValue(p);
+
+            editTextIntensity.setText("");
+            editTextLocation.setText("");
+
+            Toast.makeText(this, "Product added", Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(this, "Please enter a name", Toast.LENGTH_LONG).show();
+        }
+
 
         Toast.makeText(this, "NOT IMPLEMENTED YET", Toast.LENGTH_LONG).show();
     }
