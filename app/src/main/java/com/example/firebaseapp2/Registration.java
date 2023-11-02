edd317c51e1902d37ed0c4dd060a34477bfe75eb
Destitution <145163394+em-gee2@users.noreply.github.com>
2023-11-01 22:31:56 -0400

    Update Registration.java

--- app/src/main/java/com/example/firebaseapp2/Registration.java
+++ app/src/main/java/com/example/firebaseapp2/Registration.java
@@ -1,4 +1,50 @@
 package com.example.firebaseapp2;
 
-public class Registration {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+
+public class Registration extends AppCompatActivity {
+    EditText emailBox;
+    EditText passwordBox;
+    Button userRegister;
+    Button adminRegister;
+    DatabaseReference databaseReference;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration);
+        emailBox = (EditText) findViewById(R.id.emailSignUp);
+        passwordBox = (EditText) findViewById(R.id.passwordSignUp);
+        adminRegister = (Button) findViewById(R.id.adminSignUp);
+        userRegister = (Button) findViewById(R.id.userSignUp);
+
+        adminRegister.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        adminRegister(emailBox.getText().toString(), passwordBox.getText().toString());
+                    }
+                }
+        );
+        userRegister.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        adminRegister(emailBox.getText().toString(), passwordBox.getText().toString());
+                    }
+                }
+        );
+    }
+
+    private void adminRegister(String email, String password) {
+
+    }
+
+    private void userRegister(String email, String password) {
+
+    }
+
 }
