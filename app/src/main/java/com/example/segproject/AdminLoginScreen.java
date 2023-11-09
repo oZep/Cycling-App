0f888ad18d7b815a4d12f1811c408c57b4ac980d
Isabelle Choi <isabellelissina@gmail.com>
2023-11-09 17:20:12 -0500

    fixed registration issue, and added button ids

--- app/src/main/java/com/example/segproject/AdminLoginScreen.java
+++ app/src/main/java/com/example/segproject/AdminLoginScreen.java
@@ -1,13 +1,29 @@
 package com.example.segproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminLoginScreen extends AppCompatActivity {
+
+    Button goBackButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login_screen);
+
+        goBackButton = findViewById(R.id.goBackButton);
+
+        goBackButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
