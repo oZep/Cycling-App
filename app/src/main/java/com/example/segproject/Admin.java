d18060e547efb2a52777e0fd03d2dc67d3405ffc
Destitution <145163394+em-gee2@users.noreply.github.com>
2023-11-10 18:00:30 -0500

    Update Admin.java

--- app/src/main/java/com/example/segproject/Admin.java
+++ app/src/main/java/com/example/segproject/Admin.java
@@ -1,14 +1,21 @@
 package com.example.segproject;
 
-public class Admin extends Account{
-    public Admin(String u, String e, String p) {
-        username = u;
-        email = e;
-        password = p;
+public class Admin{
+    private String username;
+    private String password;
+    private Admin admin = new Admin();
+    private Admin() {
+        username = "admin";
+        password = "admin";
+    }
+    public Admin getInstance() {
+        return admin;
     }
 
-    public Admin(String e, String p) {
-        email = e;
-        password = p;
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
     }
 }
