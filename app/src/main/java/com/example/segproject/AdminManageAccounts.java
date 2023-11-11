6963796a6be619213ce95cb164ed59ec86e57318
Isabelle Choi <67203277+choiIsabelle@users.noreply.github.com>
2023-11-10 22:34:15 -0500

    Update AdminManageAccounts.java

--- app/src/main/java/com/example/segproject/AdminManageAccounts.java
+++ app/src/main/java/com/example/segproject/AdminManageAccounts.java
@@ -31,14 +31,13 @@ public class AdminManageAccounts extends AppCompatActivity {
 
     static AccountDBHandler db;
 
-    Button goBackToHomepage, goBackToAdminControls;
+    Button goBackToHomepage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_manage_accounts);
         goBackButton = findViewById(R.id.goBackButton);
-        goBackToAdminControls = findViewById(R.id.goToHomepageAdmin);
         accountList = findViewById(R.id.accountList);
         goBackToHomepage = findViewById(R.id.goToHomepageAdmin);
         db = new AccountDBHandler(this);
@@ -80,14 +79,6 @@ public class AdminManageAccounts extends AppCompatActivity {
         });
 
         goBackToHomepage.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Login.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        goBackToAdminControls.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), AdminLoginScreen.class);
                 startActivity(intent);
