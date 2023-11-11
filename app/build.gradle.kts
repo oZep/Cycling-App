213ab3511a9473340decec6e8ce815b301728203
choi-m <0101m744@gmail.com>
2023-11-10 22:25:27 -0500

    get rid of commented out code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,12 +48,14 @@ android {
 }
 
 dependencies {
+
     implementation("com.google.gms:google-services:4.3.10")
     implementation("com.google.android.gms:play-services-auth:19.2.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
     implementation("androidx.appcompat:appcompat-resources:1.6.1")
+    implementation("com.google.firebase:firebase-admin:9.2.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation ("androidx.constraintlayout:constraintlayout:2.1.0")
     implementation("androidx.compose.ui:ui")
@@ -62,8 +64,6 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation ("com.google.android.material:material:1.4.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-//    implementation("com.google.firebase:firebase-admin:9.2.0")
-    implementation("com.google.firebase:firebase-auth:22.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
