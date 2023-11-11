f58899bbb2db46c697a882ffc9f42633e8d09131
Isabelle Choi <isabellelissina@gmail.com>
2023-11-10 19:23:46 -0500

    Adding to michelles branch

--- app/src/main/java/com/example/segproject/AdminManageAccounts.java
+++ app/src/main/java/com/example/segproject/AdminManageAccounts.java
@@ -17,37 +17,38 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.auth.ExportedUserRecord;
+//import com.google.firebase.auth.ExportedUserRecord;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.ListUsersPage;
-import com.google.firebase.auth.UserRecord;
+//import com.google.firebase.auth.ListUsersPage;
+//import com.google.firebase.auth.UserRecord;
 
 import java.util.ArrayList;
 
 
 public class AdminManageAccounts extends AppCompatActivity {
 
-    Button goBackButton;
-<<<<<<< HEAD
-    FirebaseAuth mAuth;
+    Button goBackButton, goToHomepageAdmin;
+
+//    FirebaseAuth mAuth;
 
     ListView accountList;
-=======
+
     Button goBackToHomepage;
->>>>>>> fca05e270d1a4680a8472d406f9dbc421b88bf1c
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_manage_accounts);
-        mAuth = FirebaseAuth.getInstance();
+//        mAuth = FirebaseAuth.getInstance();
         goBackButton = findViewById(R.id.goBackButton);
-<<<<<<< HEAD
-        accountList = findViewById(R.id.accountList);
-=======
         goBackToHomepage = findViewById(R.id.goToHomepageAdmin);
->>>>>>> fca05e270d1a4680a8472d406f9dbc421b88bf1c
+
+        accountList = findViewById(R.id.accountList);
+
+        goBackToHomepage = findViewById(R.id.goToHomepageAdmin);
+
 
         goBackButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -57,45 +58,54 @@ public class AdminManageAccounts extends AppCompatActivity {
             }
         });
 
-<<<<<<< HEAD
-        ListUsersPage users = null;
-        try {
-            users = mAuth.listUsers(null);
-        } catch (FirebaseAuthException e) {
-            throw new RuntimeException(e);
-        }
-
-        ArrayList<ExportedUserRecord> arr= new ArrayList<>();
-        while (users != null) {
-            Iterator<ExportedUserRecord> iter = users.getValues().iterator();
-            while (iter.hasNext()) {
-                arr.add(iter.next());
-            }
-            users = users.getNextPage();
-        }
-
-    ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arr); accountList.setAdapter(adapter);
-        accountList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
-                    UserRecord account = (UserRecord) parent.getItemAtPosition(position);
-                    String uid = account.getUid();
-                    try {
-                        mAuth.deleteUser(uid);
-                        Toast.makeText(AdminManageAccounts.this, "User Deleted", Toast.LENGTH_SHORT).show();
-                    } catch (Exception e) {
-                        Toast.makeText(AdminManageAccounts.this, "Error Deleting User", Toast.LENGTH_SHORT).show();
-                    }
-            }
-    });
-}
-=======
-        goBackToHomepage.setOnClickListener(new View.OnClickListener() {
+        goToHomepageAdmin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), AdminLoginScreen.class);
                 startActivity(intent);
                 finish();
             }
         });
+//
+//
+//        ListUsersPage users = null;
+//        try {
+//            users = mAuth.listUsers(null);
+//        } catch (FirebaseAuthException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        ArrayList<ExportedUserRecord> arr= new ArrayList<>();
+//        while (users != null) {
+//            Iterator<ExportedUserRecord> iter = users.getValues().iterator();
+//            while (iter.hasNext()) {
+//                arr.add(iter.next());
+//            }
+//            users = users.getNextPage();
+//        }
+//
+//    ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arr); accountList.setAdapter(adapter);
+//        accountList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
+//                    UserRecord account = (UserRecord) parent.getItemAtPosition(position);
+//                    String uid = account.getUid();
+//                    try {
+//                        mAuth.deleteUser(uid);
+//                        Toast.makeText(AdminManageAccounts.this, "User Deleted", Toast.LENGTH_SHORT).show();
+//                    } catch (Exception e) {
+//                        Toast.makeText(AdminManageAccounts.this, "Error Deleting User", Toast.LENGTH_SHORT).show();
+//                    }
+//            }
+//    });
+//}
+//
+//        goBackToHomepage.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View view) {
+//                Intent intent = new Intent(getApplicationContext(), AdminLoginScreen.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
+//    }
+
     }
->>>>>>> fca05e270d1a4680a8472d406f9dbc421b88bf1c
 }
