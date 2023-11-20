df17ba7e1b15519ece257256bd09a990be14b700
choiisabelle <isabellelissina@gmail.com>
2023-11-19 20:32:51 -0500

    added club admin profile page, design implemented

---
+++ app/src/main/java/com/example/segproject/ClubAdminProfilePage.java
@@ -0,0 +1,14 @@
+package com.example.segproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClubAdminProfilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_club_admin_profile_page);
+    }
+}
