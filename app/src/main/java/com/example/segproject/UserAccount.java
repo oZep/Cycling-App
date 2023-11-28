a9d30ea0774fcbea8921b64004b2354b63a49e07
Destitution <145163394+em-gee2@users.noreply.github.com>
2023-11-27 19:23:10 -0500

    Update UserAccount.java

--- app/src/main/java/com/example/segproject/UserAccount.java
+++ app/src/main/java/com/example/segproject/UserAccount.java
@@ -5,13 +5,11 @@ import java.util.Map;
 
 public abstract class UserAccount {
     protected String username;
-    protected String email;
     protected String password;
 
     public Map<String, Object> toMap() {
         HashMap<String, Object> result = new HashMap<String, Object>();
         result.put("username", username);
-        result.put("email", email);
         result.put("password", password);
         return result;
     }
@@ -19,10 +17,7 @@ public abstract class UserAccount {
     public String getUsername() {
         return username;
     }
-
-    public String getEmail() {
-        return email;
-    }
+    
 
     public String getPassword() {
         return password;
