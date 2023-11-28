2161c06dcb67a7e5752f1e1d848c9f3a439121c0
Destitution <145163394+em-gee2@users.noreply.github.com>
2023-11-27 19:09:50 -0500

    Update ClubOwner.java

--- app/src/main/java/com/example/segproject/ClubOwner.java
+++ app/src/main/java/com/example/segproject/ClubOwner.java
@@ -1,14 +1,34 @@
 package com.example.segproject;
 
 public class ClubOwner extends UserAccount{
-    public ClubOwner(String u, String e, String p) {
-        username = u;
+    private String socialMedia;
+    private String contact;
+    private String phoneNum;
+
+    public ClubOwner(String e, String p, String sm, String c, String pn) {
         email = e;
         password = p;
+        socialMedia = sm;
+        contact = c;
+        phoneNum = pn;
     }
 
-    public ClubOwner(String e, String p) {
+    public ClubOwner(String e, String p, String sm, String pn) {
         email = e;
         password = p;
+        socialMedia = sm;
+        phoneNum = pn;
+    }
+
+    public String getSocialMedia() {
+        return socialMedia;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
     }
 }
