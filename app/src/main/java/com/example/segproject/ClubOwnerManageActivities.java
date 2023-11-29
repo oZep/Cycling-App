6dace0bd6a241ba9cd6fcb515b7a5e0c11aaeffb
Destitution <squidkiller64@hotmail.com>
2023-11-28 22:47:55 -0500

    m

--- app/src/main/java/com/example/segproject/ClubOwnerManageActivities.java
+++ app/src/main/java/com/example/segproject/ClubOwnerManageActivities.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ClubOwnerManageActivites extends AppCompatActivity {
+public class ClubOwnerManageActivities extends AppCompatActivity {
     Button goBackButton, goBackToHomepage, goToAddEventPage, viewEvents, deleteEvent, editEvents;
     String clubName;
     @Override
@@ -15,76 +15,41 @@ public class ClubOwnerManageActivites extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_club_owner_manage_activites);
         goBackButton = findViewById(R.id.goBackButton);
-        goBackToHomepage = findViewById(R.id.goToHomepageAdmin);
         goToAddEventPage = findViewById(R.id.addevent);
         viewEvents = findViewById(R.id.viewEvents_btn);
-        deleteEvent = findViewById(R.id.deleteEvent_btn);
-        editEvents = findViewById(R.id.editEvent);
 
         Intent intent = getIntent();
         clubName = intent.getStringExtra("clubName");
 
-
-
-
-        deleteEvent.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), DeleteEventPage.class);
-                intent.putExtra("clubName", clubName);
-                startActivity(intent);
-                finish();
-            }
-        });
         goBackButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), ClubOwnerLogin.class);
+                Intent intent = new Intent(getApplicationContext(), Login.class);
                 startActivity(intent);
                 finish();
             }
         });
 
-        goBackToHomepage.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
 
         goToAddEventPage.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), AddEventPage.class);
                 startActivity(intent);
+                intent.putExtra("clubName", clubName);
                 finish();
             }
         });
 
-        deleteEvent.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), DeleteEventPage.class);
-                intent.putExtra("clubName", clubName);
-                startActivity(intent);
-                finish();
-            }
-        });
 
         viewEvents.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), EventAdapter.class);
                 startActivity(intent);
+                intent.putExtra("clubName", clubName);
                 finish();
             }
         });
 
-        editEvents.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), EditEvent.class);
-                intent.putExtra("clubName", clubName);
-                startActivity(intent);
-                finish();
-            }
-        });
     }
 }
 
