4a4a1390c0dcc21c78eba92285c606c6b39293ab
Destitution <squidkiller64@hotmail.com>
2023-11-28 20:57:59 -0500

    stuff

--- app/src/main/java/com/example/segproject/AddEventPage.java
+++ app/src/main/java/com/example/segproject/AddEventPage.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class activity_admin_addevent_page extends AppCompatActivity {
+public class AddEvent extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
