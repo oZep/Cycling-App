450ddfdc6ecd60bf5c19b144903c1b32e771ea26
joeyissa <joeyissa59@gmail.com>
2023-11-28 23:16:16 -0500

    class imported

--- app/src/main/java/com/example/segproject/EventList.java
+++ app/src/main/java/com/example/segproject/EventList.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
