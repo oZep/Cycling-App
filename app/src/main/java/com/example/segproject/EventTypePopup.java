3f8e072f019a3cee1fa903e8f8c40ad16728bcf7
Destitution <squidkiller64@hotmail.com>
2023-12-06 00:27:15 -0500

    adding

---
+++ app/src/main/java/com/example/segproject/EventTypePopup.java
@@ -0,0 +1,56 @@
+package com.example.segproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EventTypePopup extends AppCompatActivity {
+    Button goBackButton, goBackToHomepage, deleteEventType, editEventType;
+    String eventTypeN;
+    EventTypeDBHandler db;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_popup);
+        goBackButton = findViewById(R.id.goBackButton);
+        goBackToHomepage = findViewById(R.id.goToHomepageAdmin);
+        deleteEventType = findViewById(R.id.deleteEvent_btn);
+        editEventType = findViewById(R.id.editEvent);
+        db = new EventTypeDBHandler(this);
+        Intent intent = getIntent();
+        eventTypeN = intent.getStringExtra("eventTypeN");
+
+
+        deleteEventType.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                db.deleteEventType(eventTypeN);
+                Intent intent = new Intent(getApplicationContext(), AdminViewEventTypes.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        editEventType.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), EditEventType.class);
+                intent.putExtra("eventN", eventTypeN);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        goBackToHomepage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AdminViewEventTypes.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
