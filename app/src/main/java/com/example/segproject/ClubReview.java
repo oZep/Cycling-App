694695d689f8f3f4d3e92dff5681c6404c60b6ad
Destitution <squidkiller64@hotmail.com>
2023-12-05 19:36:10 -0500

    commenting

--- app/src/main/java/com/example/segproject/ClubReview.java
+++ app/src/main/java/com/example/segproject/ClubReview.java
@@ -4,17 +4,23 @@ public class ClubReview {
     private Participant rater;
     private Club ratee;
     private int rating;
+    private String comment;
 
-    public ClubReview(Participant p, Club c, int stars) {
+    public ClubReview(Participant p, Club c, int stars, String words) {
         rater = p;
         ratee = c;
         rating = stars;
+        comment = words;
         rater.addReview(c, this);
         ratee.addReview(this);
     }
 
     public int getRating() {
         return rating;
+    }
+
+    public String getComment() {
+        return comment;
     }
 
     public Participant getRater() {
