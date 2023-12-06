16eff0af09cccbf0f74c1e90152dee7c6eda7752
joeyissa <joeyissa59@gmail.com>
2023-12-05 19:04:43 -0500

    sending username to register and rate + rateclub

--- app/src/main/java/com/example/segproject/RateClub.java
+++ app/src/main/java/com/example/segproject/RateClub.java
@@ -1,4 +1,13 @@
 package com.example.segproject;
 
+import android.os.Bundle;
+
 public class RateClub {
+    
+    String username;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        goBackBtn = findViewById(R.id.goBack);
+        name = findViewById(R.id.name);
 }
